5601103199d73094d2e08fe734e61058537d9ea0
bpham <brianthongpham@gmail.com>
2017-10-28 17:46:31 -0700

    made lab3.py look nicer

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -13,7 +13,7 @@ print(array)
 
 k = (int(input("\n\nEnter a number between 1 and the number you picked: ")))
 while(k > n):
-    k = (int(input("\n\nEnter a number between 1 and the number you picked: ")))
+    k = (int(input("\nEnter a number between 1 and the number you picked: ")))
 
 sys.setrecursionlimit(10000000)
 print("The " + str(k) + "th least number is: " + str(quickSearch(array, k)) + "\n\n")
