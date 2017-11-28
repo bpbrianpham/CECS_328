b370d08cc8eccbf43e83dfdfb605c02dec9acae8
bpham <brianthongpham@gmail.com>
2017-11-27 16:27:42 -0800

    fixed bug issues in the O(n)

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -1,15 +1,17 @@
 import random
 import sys
-import mss
+from mss import *
 
 n = (int(input("\nEnter a positive number: ")))
 
 while (n < 0):
     n = (int(input("\nEnter a positive number: ")))
 
-a = [0] * n
+array = [0] * n
 
 for i in range(n):
     array[i] = random.randint(-100, 100)
 
-print(mssBigOofN(a))
+print(array)
+
+print(mssBigOofN(array))
--- Lab4/mss.py
+++ Lab4/mss.py
@@ -2,11 +2,12 @@ def mssBigOofN(a):
     temp = a[0]
     mss = temp
     for i in range(1, len(a)):
+        if (temp < 0):
+            temp = 0
         temp = temp + a[i]
         if (mss < temp):
             mss = temp
-        if (temp < 0):
-            temp = 0
+        
     return mss
 
-def mssBigOofLogN(a):
+#def mssBigOofLogN(a):
