b397e06f59b0e1b5fbfc17ac88e0754aab5258e1
bpham <brianthongpham@gmail.com>
2017-12-04 18:11:23 -0800

    fixed selection sort algorithm

--- Lab5/SelectionSort.py
+++ Lab5/SelectionSort.py
@@ -2,7 +2,7 @@ def selectionSort(array):
     for i in range(len(array)):
         minimum = i
         for j in range(i + 1, len(array)):
-            if (array[j] < array[i]): 
+            if (array[j] < array[minimum]): 
                 minimum = j
         array[minimum], array[i] = array[i], array[minimum]
     return array        
